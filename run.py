93475ab843e4a4dc9cef526cd01f08e8550df07d
iulianpro <iulian.prodea@gmail.com>
2020-03-27 01:11:10 +0000

    Checked MongoDB connection, GET 200

--- run.py
+++ run.py
@@ -1,9 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def home():
-    return "Hello, I am your app (test)"
+@app.route("/get_shifts")
+def get_shifts():
+    return render_template("shifts.html", shifts=mongo.db.shifts.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
