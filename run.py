fda4234dcad17c2b64890cb0e26293e42138f65d
iulianpro <iulian.prodea@gmail.com>
2020-03-28 01:51:16 +0000

    Added route decorator for landing page

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
