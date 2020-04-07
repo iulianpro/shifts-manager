c49bf41f2784ebf2600c8e9c12b7e0a6ba5cd8a7
iulianpro <iulian.prodea@gmail.com>
2020-04-07 04:07:45 +0100

    Added js to autohide collapse navbar on click nav item

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
         $('#loading').removeClass('d-none');
         $('#loading').addClass('d-inline');
     });
+
+    $('.navbar-nav li').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 });
