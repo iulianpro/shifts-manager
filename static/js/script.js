e98e441b2d59b56d21617662d3f638e410d97b1a
iulianpro <iulian.prodea@gmail.com>
2020-04-12 12:33:47 +0100

    Added confirmation message at delete employee

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
-
     $("[title|='Delete shift']").confirm({
         columnClass: 'col-md-4 col-md-offset-4',
         draggable: false,
@@ -52,6 +51,37 @@ $(document).ready(function () {
             },
             Edit: {
                 btnClass: 'btn-warning',
+                action: function () {
+                    $.confirm({
+                        boxWidth: '72px',
+                        useBootstrap: false,
+                        icon: 'fa fa-spinner fa-spin',
+                        draggable: false,
+                        title: false,
+                        buttons: {
+                            yes: {
+                                isHidden: true
+                            }
+                        }
+                    });
+                    location.href = this.$target.attr('href');
+                }
+            }
+        }
+    });
+
+    $("[title^='Delete employee']").confirm({
+        columnClass: 'col-md-4 col-md-offset-4',
+        draggable: false,
+        backgroundDismissAnimation: 'glow',
+        type: 'red',
+        typeAnimated: true,
+        buttons: {
+            Cancel: {
+                btnClass: 'btn-default'
+            },
+            Delete: {
+                btnClass: 'btn-red',
                 action: function () {
                     $.confirm({
                         boxWidth: '72px',
