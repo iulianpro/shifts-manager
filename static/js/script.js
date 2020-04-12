92967ef2e27ac69f847b6d628823d6c42995e251
iulianpro <iulian.prodea@gmail.com>
2020-04-12 11:07:19 +0100

    Added confirmation message at delete ona entire shift

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,37 @@ $(document).ready(function () {
     $('.navbar-nav li').click(function () {
         $('.navbar-collapse').collapse('hide');
     });
+
+
+    $("[title|='Delete shift']").confirm({
+        columnClass: 'col-md-4 col-md-offset-4',
+        draggable: false,
+        backgroundDismissAnimation: 'glow',
+        type: 'red',
+        typeAnimated: true,
+        buttons: {
+            Cancel: {
+                btnClass: 'btn-default'
+            },
+            Delete: {
+                btnClass: 'btn-red',
+                action: function () {
+                    $.confirm({
+                        icon: 'fa fa-spinner fa-spin',
+                        draggable: false,
+                        defaultButtons: false,
+                        title: false,
+                        smoothContent: false,
+                        content: false,
+                        buttons: {
+                            yes: {
+                                isHidden: true
+                            }
+                        }
+                    });
+                    location.href = this.$target.attr('href');
+                }
+            }
+        }
+    });
 });
