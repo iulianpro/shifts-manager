ca323d3b3d30d2948432a6fc78f713c195956223
iulianpro <iulian.prodea@gmail.com>
2020-04-15 01:51:21 +0100

    Deleted duplicate js code

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
-    $("[title|='Delete shift'],[title^='Erase']").confirm({
+    $("[title|='Delete shift'],[title^='Erase'],[title^='Delete employee']").confirm({
         animateFromElement: false,
         animation: 'zoom',
         columnClass: 'col-md-4 col-md-offset-4',
@@ -57,41 +57,6 @@ $(document).ready(function () {
             },
             Edit: {
                 btnClass: 'btn-warning',
-                action: function () {
-                    $.confirm({
-                        animateFromElement: false,
-                        animation: 'zoom',
-                        boxWidth: '72px',
-                        useBootstrap: false,
-                        icon: 'fa fa-spinner fa-spin',
-                        draggable: false,
-                        title: false,
-                        buttons: {
-                            yes: {
-                                isHidden: true
-                            }
-                        }
-                    });
-                    location.href = this.$target.attr('href');
-                }
-            }
-        }
-    });
-
-    $("[title^='Delete employee']").confirm({
-        animateFromElement: false,
-        animation: 'zoom',
-        columnClass: 'col-md-4 col-md-offset-4',
-        draggable: false,
-        backgroundDismissAnimation: 'glow',
-        type: 'red',
-        typeAnimated: false,
-        buttons: {
-            Cancel: {
-                btnClass: 'btn-default'
-            },
-            Delete: {
-                btnClass: 'btn-red',
                 action: function () {
                     $.confirm({
                         animateFromElement: false,
